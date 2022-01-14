22a7f00dfd8ccad9068eaf1cf449b080058aba13
aericknunez <aerick.nunez@gmail.com>
2022-01-14 10:02:48 -0600

    Genesis

--- facturas/45/Impresiones.php
+++ facturas/45/Impresiones.php
@@ -133,19 +133,219 @@ $printer->close();
 
 
 
- public function Factura($data){
-
-}   /// termina FACTURA
+public function Factura($data){
 
 
+  $txt1   = "15"; 
+  $txt2   = "5";
+  $txt3   = "0";
+  $txt4   = "0";
+  $n1   = "16";
+  $n2   = "60";
+  $n3   = "30";
+  $n4   = "0";
+  
+  
+  $col1 = 38;
+  $col2 = 89;
+  $col3 = 370;
+  $col4 = 550;
+  $col5 = 500;
+  // $print
+  $print = "FACTURA";
+  
+  $handle = printer_open($print);
+  printer_set_option($handle, PRINTER_MODE, "RAW");
+  
+  printer_start_doc($handle, "Mi Documento");
+  printer_start_page($handle);
+  
+  
+  $font = printer_create_font("Arial", $txt1, $txt2, PRINTER_FW_NORMAL, false, false, false, 0);
+  printer_select_font($handle, $font);
+  
+  
+  
+  $oi=80;
+  //// comienza la factura
+  
+  
+  
+  $oi=$oi+$n1;
+  printer_draw_text($handle, $data["nombre"], 95, $oi+$n1);
+  printer_draw_text($handle, date("d") . " " . Fechas::MesEscrito(date("m")) ."" . date("Y"), 400, $oi);
+  
+  $oi=$oi+$n1;
+  printer_draw_text($handle, $data["direccion"], 110, $oi+$n1);
+  
+  printer_draw_text($handle, $data["documento"], 110, $oi+$n1+$n1);
+  
+  
+  $oi=174; // salto de linea
+  
+  
+      foreach ($data["productos"] as $producto) {
+   
+  
+            $oi=$oi+$n1;
+            printer_draw_text($handle, $producto["cant"], $col1, $oi);
+            printer_draw_text($handle, $producto["producto"], $col2, $oi);
+            printer_draw_text($handle, $producto["pv"], $col3, $oi);
+            printer_draw_text($handle, $producto["total"], $col4, $oi);
+  
+      } 
+  
+  
+  
+  
+  
+  /// salto de linea
+  $oi=427;
+  
+  // valores en letras
+  printer_draw_text($handle, Dinero::DineroEscrito($data["total"]), $col2, $oi);
+  // echo wordwrap($cadena, 15, "<br>" ,FALSE);
+  
+  // volores numericos
+  printer_draw_text($handle, Helpers::Format($data["total"]), $col4, $oi);
+  
+  
+  $oi=$oi+$n1+$n1;
+  printer_draw_text($handle, Helpers::Format($data["total"]), $col4, $oi);
+  
+  
+  $oi=$oi+$n1+32;
+  printer_draw_text($handle, Helpers::Format($data["total"]), $col4, $oi);
+  
+  
+  printer_delete_font($font);
+  
+  printer_end_page($handle);
+  printer_end_doc($handle);
+  printer_close($handle);
+  
+  
+  }   /// termina FACTURA
+  
+  
+  
+  
+  
+   public function CreditoFiscal($data){
+  
+      $txt1   = "15"; 
+      $txt2   = "5";
+      $txt3   = "0";
+      $txt4   = "0";
+      $n1   = "16";
+      $n2   = "60";
+      $n3   = "30";
+      $n4   = "0";
+      
+      
+      $col1 = 38;
+      $col2 = 89;
+      $col3 = 370;
+      $col4 = 550;
+      $col5 = 500;
+      // $print
+      $print = "FACTURA";
+  
+  $handle = printer_open($print);
+  printer_set_option($handle, PRINTER_MODE, "RAW");
+  
+  printer_start_doc($handle, "Mi Documento");
+  printer_start_page($handle);
+  
+  
+  $font = printer_create_font("Arial", $txt1, $txt2, PRINTER_FW_NORMAL, false, false, false, 0);
+  printer_select_font($handle, $font);
+  
+  
+  
+  $oi=74;
+  //// comienza la factura
+  
+  
+  
+  $oi=$oi+$n1;
+  printer_draw_text($handle, $data["cliente"], 95, $oi);
+  printer_draw_text($handle, date("d") . " " . Fechas::MesEscrito(date("m")) ."" . date("Y"), 400, $oi);
+  
+  $oi=$oi+$n1;
+  printer_draw_text($handle, $data["registro"], 430, $oi);
 
+  $oi=$oi+$n1;
+  printer_draw_text($handle, $data["direccion"], 100, $oi);
+  printer_draw_text($handle, $data["documento"], 400, $oi);
 
+  $oi=$oi+$n1;
+  printer_draw_text($handle, $data["giro"], 400, $oi);
 
- public function CreditoFiscal($data){
-
-
-}
-
+  $oi=$oi+$n1;
+  printer_draw_text($handle, $data["departamento"], 130, $oi);
+  
+  $oi=$oi+$n1;
+  printer_draw_text($handle, "CONTADO", 215, $oi);
+  
+  
+  
+  $oi=191; // salto de linea
+  
+  
+      foreach ($data["productos"] as $producto) {
+   
+  
+        $oi=$oi+$n1;
+        printer_draw_text($handle, $producto["cant"], $col1, $oi);
+        printer_draw_text($handle, $producto["producto"], $col2, $oi);
+        printer_draw_text($handle, Helpers::Format4D(Helpers::STotal($producto["pv"], $data['config_imp'])), $col3, $oi);
+        printer_draw_text($handle, $producto["stotal"], $col4, $oi);
+  
+      } 
+  
+  
+  
+  /// salto de linea
+  $oi=405;
+  
+  // valores en letras
+  printer_draw_text($handle, Dinero::DineroEscrito($data["total"]), $col2, $oi);
+  // echo wordwrap($cadena, 15, "<br>" ,FALSE);
+  
+  
+  // volores numericos
+  // printer_draw_text($handle, Helpers::Format($totalx), $col4, $oi);
+  printer_draw_text($handle, Helpers::Format($data["stotal"]), $col4, $oi);
+  
+  
+  
+  
+  $oi=$oi+$n1;
+  // printer_draw_text($handle, Helpers::Format($impx), $col4, $oi);
+  printer_draw_text($handle, Helpers::Format(Helpers::Impuesto(Helpers::STotal($data["total"], $data['config_imp']), $data['config_imp'])), $col4, $oi);
+  
+  
+  $oi=$oi+$n1;
+  printer_draw_text($handle, Helpers::Format($data["total"]), $col4, $oi);
+  
+  
+  $oi=$oi+$n1+$n1+$n1+10;
+  printer_draw_text($handle, Helpers::Format($data["total"]), $col4, $oi);
+  
+  
+  printer_delete_font($font);
+  ///
+  printer_end_page($handle);
+  printer_end_doc($handle);
+  printer_close($handle);
+  
+  
+  }
+  
+  
+  
+  
 
 
 
